14db58ebdcc4d8317173726da4aca9f01e2dfc7b
alin <248628578@qq.com>
2018-02-11 09:29:03 +0800

    add  KafkaLogMessage and MessageContext by kafka

---
+++ Src/CommonX/Models/LogMessageEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonX.Models
+{
+    public enum KafkaLogType
+    {
+        ConsumeError,
+        ServerConnError
+    }
+    public class LogMessageEventArgs: EventArgs
+    {
+        public string Reason { get; set; }
+
+        public KafkaLogType LogType { get; set; }
+    }
+}
---
+++ Src/CommonX/Models/MessageContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonX.Models
+{
+    /// <summary>
+    /// Message context
+    /// </summary>
+    public class MessageContext
+    {
+        /// <summary>
+        /// Gets or sets the message group.
+        /// </summary>
+        public string Group { get; set; }
+
+        /// <summary>
+        /// Message name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Message content
+        /// </summary>
+        public string Content { get; set; }
+
+        public override string ToString()
+        {
+            return $"Group:{Group}, Name:{Name}, Content:{Content}";
+        }
+    }
+}
